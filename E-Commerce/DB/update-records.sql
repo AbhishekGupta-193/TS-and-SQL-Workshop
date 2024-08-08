3bef29dd02d7f491d4654d970f0d226428644d4a
ABHISHEK KR. GUPTA <abhishekkgupta20_ug@ece.nits.ac.in>
2024-08-08 15:27:37 +0530

    Added:Trigger on delete event and adds deleted entry on Backup table

--- E-Commerce/DB/update-records.sql
+++ E-Commerce/DB/update-records.sql
@@ -19,3 +19,32 @@ insert into Products(id,name,price,in_stock,review,avg_rating) values(4,'Rainy S
 delete from Products where id=4;
 
 select * from Products;
+
+
+------------- PRODUCTS_BACKUP TABLE CREATED-----------------
+Create Table Products_Backup(
+	id int,
+	name char(20),
+	price int,
+	in_stock int,
+	review char(50),
+	avg_rating int,
+	deleted_at timestamp default current_timestamp
+);
+
+select * from Products_Backup;
+
+---------------TRIGGER FUNCTION CREATED----------------------
+create or replace function after_product_delete()
+returns trigger as $$
+begin
+insert into Products_Backup(id,name,price,in_stock,review,avg_rating,deleted_at)
+values (OLD.id,OLD.name,OLD.price,OLD.in_stock,OLD.review,OLD.avg_rating,current_timestamp);
+return OLD;
+end;$$ language plpgsql;
+
+---------------TRIGGER CREATED----------------------
+create trigger after_product_delete
+after delete on Products
+for each row
+execute function after_product_delete();
