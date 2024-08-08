50f1f53e76997719d5cafc098c32d3ba7ef9f304
ABHISHEK KR. GUPTA <abhishekkgupta20_ug@ece.nits.ac.in>
2024-08-08 12:01:14 +0530

    Added:Place Order functionality in services

--- E-Commerce/TS/build/services/service.js
+++ E-Commerce/TS/build/services/service.js
@@ -12,5 +12,23 @@ const product1 = new product_1.Product("Polo TShirt", 600, 10, ["Good", "Worth i
 add_products(product1);
 const product2 = new product_1.Product("Cotton Trouser", 950, 6, ["Great fit", "Best", "Awesome"], 4.9);
 add_products(product2);
-const product3 = new product_1.Product("Slipper", 700, 18, ["Worse", "Defective", "Average"], 2.6);
+const product3 = new product_1.Product("Rainy Slipper", 300, 18, ["Worse", "Defective", "Average"], 2.6);
 add_products(product3);
+const product4 = new product_1.Product("Rainy Slipper", 700, 8, ["Comfortable", "Durable", "Good"], 4.6);
+add_products(product4);
+const Order_List = [];
+function place_order(commodity, quantity) {
+    let flag = false;
+    Products_List.forEach((element) => {
+        if (element.name == commodity && element.avg_rating > 4.0 && element.in_stock >= quantity) {
+            Order_List.push(element);
+            flag = true;
+            console.log(`Order for ${element.name} is placed...`);
+        }
+    });
+    if (!flag)
+        console.log(`Sorry, ${commodity} is currently out of stock...`);
+}
+place_order("Casual TShirt", 3);
+place_order("Rainy Slipper", 1);
+console.log("The Products Ordered are : ", Order_List);
--- E-Commerce/TS/src/services/service.ts
+++ E-Commerce/TS/src/services/service.ts
@@ -13,11 +13,26 @@ const product1=new Product("Polo TShirt",600,10,["Good","Worth it","Not Bad"],4.
 add_products(product1);
 const product2=new Product("Cotton Trouser",950,6,["Great fit","Best","Awesome"],4.9);
 add_products(product2);
-const product3=new Product("Rainy Slipper",700,18,["Worse","Defective","Average"],2.6);
+const product3=new Product("Rainy Slipper",300,18,["Worse","Defective","Average"],2.6);
 add_products(product3);
+const product4=new Product("Rainy Slipper",700,8,["Comfortable","Durable","Good"],4.6);
+add_products(product4);
 
 
-
-
+const Order_List:Product[]=[];
+function place_order(commodity:string,quantity:number):void{
+    let flag:boolean=false;
+    Products_List.forEach((element)=>{
+        if(element.name==commodity && element.avg_rating>4.0 && element.in_stock>=quantity){
+            Order_List.push(element);
+            flag=true;
+            console.log(`Order for ${element.name} is placed...`);
+        }
+    })
+    if(!flag)console.log(`Sorry, ${commodity} is currently out of stock...`);
+}
+place_order("Casual TShirt",3);
+place_order("Rainy Slipper",1);
+console.log("The Products Ordered are : ",Order_List);
 
 
